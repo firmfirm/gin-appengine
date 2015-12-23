b39ce6d953797c66319ad8201f6bbd750d158b21
Martynas Kazlauskas <martynas@firmfirm.co>
2015-12-23 20:52:55 +0200

    dont do recovery

--- gingae.go
+++ gingae.go
@@ -1,6 +1,7 @@
 package gingae
 
 import (
+	"fmt"
 	"runtime"
 
 	"github.com/gin-gonic/gin"
@@ -30,21 +31,18 @@ func GaeToGinHandler(handler GaeHandlerFunc) gin.HandlerFunc {
 // GaeErrorLogger adds GAE error/warning logger.
 // If there were any errors, will:
 // Calls `Warningf` if returned status code is < 500, otherwise `Errorf`
-// `Criticalf`, if there was a panic
 var GaeErrorLogger = GaeToGinHandler(func(c *gin.Context, gae appengine.Context) {
-	defer func() {
-		if err := recover(); err != nil {
-			stack := make([]byte, 1<<16)
-			runtime.Stack(stack, true)
-			gae.Criticalf("%s\nStacktrace:%s\n", err, stack)
-		}
-	}()
 	c.Next()
 	for _, err := range c.Errors {
 		msg := err.Error()
 		if c.Writer.Status() < 500 {
 			gae.Warningf(msg)
 		} else {
+			if gin.Mode() == gin.DebugMode {
+				stack := make([]byte, 1<<16)
+				runtime.Stack(stack, true)
+				msg = fmt.Sprintf("%s\nStacktrace:%s\n", err, stack)
+			}
 			gae.Errorf(msg)
 		}
 	}
